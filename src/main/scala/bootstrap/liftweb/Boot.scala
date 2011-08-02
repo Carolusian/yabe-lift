29884fab288439dd4a1ae4a126d4dba1d3e56ab0
pkufashuo400 <pkufashuo400@gmail.com>
2011-08-02 18:32:03 +0800

    finished day 3

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -42,7 +42,7 @@ class Boot {
     // Build SiteMap
     def sitemap = SiteMap(
       Menu.i("Home") / "index" >> User.AddUserMenusAfter, // the simple way to declare a menu
-
+      Menu.i("Read") /  "read" ,
       // more complex because this menu allows anything in the
       // /static path to be visible
       Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
