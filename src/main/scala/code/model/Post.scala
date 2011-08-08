c4cebbca7ebca75c4e689e6a9cc80060a4585c1d
pkufashuo400 <pkufashuo400@gmail.com>
2011-08-08 10:53:31 +0800

    add author foreign key

--- src/main/scala/code/model/Post.scala
+++ src/main/scala/code/model/Post.scala
@@ -1,10 +1,16 @@
 package code.model
 
 import net.liftweb.mapper._
+import net.liftweb.common.Full
 
 class Post extends LongKeyedMapper[Post] with IdPK {
   def getSingleton = Post
 
+  object author extends MappedLongForeignKey(this, User) {
+    override def validSelectValues() = {
+      Full(User.findAll.map(u => (u.id.get,u.firstName.get + " " + u.lastName.get)))
+    }
+  }
   object title extends MappedString(this, 140)
   object content extends MappedTextarea(this, 65535)
   object postedAt extends MappedDate(this)
